dd58a38d37efc78f04175c8046d6801b82b9e375
navateja5994 <navatejar80@gmail.com>
2024-12-04 12:32:13 +0530

    Add files via upload

---
+++ fourpow.java
@@ -0,0 +1,17 @@
+class bitwise
+{
+    public static void main(String args[])
+    {
+        
+        int n=10;
+        if((n&(n-2))==0)
+        {
+        System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+
+    }
+}      
