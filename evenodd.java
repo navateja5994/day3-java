56795e8d3ca9bc6477f06548974ae1f6f2415cf4
navateja5994 <navatejar80@gmail.com>
2024-12-04 10:20:05 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,16 @@
+class bitwise
+{
+    public static void main(String args[])
+    {
+     int a=17;
+     if((a&1)==0)
+     {
+        System.out.println("EVEN");
+     }
+     else
+     {
+        System.out.println("ODD");
+     }
+
+    }
+}
