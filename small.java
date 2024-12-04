95e542a96d4e15ad92ea97644a3d7b791c7b58aa
navateja5994 <navatejar80@gmail.com>
2024-12-04 15:06:49 +0530

    Add files via upload

---
+++ small.java
@@ -0,0 +1,21 @@
+class three
+{
+    public static void main(String args[])
+    {
+        int a=2;
+        int b=27;
+        int c=87;
+        if((a>b)&(a>c))
+        {System.out.println(" a is big");
+        }
+        else if((b>a)&(b>c))
+        {
+            System.out.println("c is big");
+        }
+        else
+        {
+            System.out.println("c is big");
+        }
+    }
+    
+}
